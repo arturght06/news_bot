a75fc4a924f558f21de2a4dabf1deddce29a2c34
Artur <01194240@pw.edu.pl>
2024-02-14 10:34:25 +0100

    first release

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
-api_id = 20617068
-api_hash = '9569315c4f7d18fd82c11d7c8f45575a'
-phone = '+48697255910'
+import os
+from dotenv import load_dotenv
 
-token = '5546594390:AAEpEMSF69_5JLapZ-A6rBNq9u7tEF2bR7I'
+load_dotenv(".env")
 
-admin_id = [904245039]
-alert_channel_id = -1001551885182
+token: str = os.getenv("TOKEN")
+admin_id = [int(i) for i in os.getenv("ADMIN_ID")[1:-1].split(",")]
+adminPanelPasswords = [i.replace("\"", "").replace(" ", "") for i in os.getenv("ADMIN_PASSWORDS")[1:-1].split(",")]
+mongodb_cluster_link: str = os.getenv("MONGODB_CLUSTER_LINK")
+
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+phone = os.getenv("PHONE")
+
+alert_channel_id = os.getenv("ALERT_CHANNEL_ID")
